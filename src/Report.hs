d814332aabffd1578c36775d7e1e144e10a2fa88
William McPherson <willmcpherson2@gmail.com>
2022-04-10 14:50:43 +1000

    Simplify Report.hs

--- src/Report.hs
+++ src/Report.hs
@@ -2,8 +2,7 @@ module Report (Report (..), Message (..), getMessages) where
 
 import Ast hiding (Token)
 import Data.List (intercalate)
-import Data.List.Extra (firstJust)
-import Data.Maybe (fromJust)
+import Data.List.Extra (splitOn)
 import Prelude hiding (error, lines)
 
 class Report a where
@@ -32,52 +31,37 @@ instance Report Name where
 --------------------------------------------------------------------------------
 
 data Message = Message
-  { startLine :: Pos
-  , startColumn :: Pos
-  , endLine :: Pos
-  , endColumn :: Pos
+  { lineNum :: Pos
+  , columnNum :: Pos
   , text :: String
   , error :: Error
   }
   deriving (Show)
 
 instance Display Message where
-  display Message{startLine, startColumn, endLine, endColumn, error, text} =
+  display Message{lineNum, columnNum, error, text} =
     intercalate
       "\n"
-      [ displaySpan startLine startColumn endLine endColumn
+      [ displaySpan lineNum columnNum
       , display error
-      , ""
-      , pointAt startColumn
+      , pointAt columnNum
       , text
       ]
 
 instance Display [Message] where
   display = intercalate "\n\n" . map display
 
-displaySpan :: Pos -> Pos -> Pos -> Pos -> String
-displaySpan startLine startColumn endLine endColumn =
-  "error at "
-    ++ displayPos startLine startColumn
-    ++ "-"
-    ++ displayPos endLine endColumn
+displaySpan :: Pos -> Pos -> String
+displaySpan lineNum columnNum =
+  "error at " ++ displayPos lineNum columnNum
 
 displayPos :: Pos -> Pos -> [Char]
-displayPos line column = show line ++ ":" ++ show column
+displayPos line column = show (line + 1) ++ ":" ++ show (column + 1)
 
 pointAt :: Pos -> String
 pointAt pos = replicate pos ' ' ++ "↓"
 
 --------------------------------------------------------------------------------
-
-type CharInfo = (Char, Info)
-
-data Info = Info
-  { infoPos :: Pos
-  , infoLine :: Pos
-  , infoColumn :: Pos
-  }
-  deriving (Show)
 
 getMessages :: String -> [Error] -> [Message]
 getMessages source = map (getMessage source)
@@ -85,56 +69,29 @@ getMessages source = map (getMessage source)
 getMessage :: String -> Error -> Message
 getMessage source error =
   let span = getSpan error
-      charInfos = getCharInfos source
-      (start, end) = spanToInfoSpan span charInfos
-      text = getLinesOf (infoLine start) (infoLine end) charInfos
-   in Message
-        { startLine = infoLine start
-        , startColumn = infoColumn start
-        , endLine = infoLine end
-        , endColumn = infoColumn end
-        , error
-        , text
-        }
+      Sections{leftLines, prefix, mid, suffix} = getSections span source
+      lineNum = length leftLines
+      columnNum = length prefix
+      text = prefix ++ mid ++ suffix
+   in Message{lineNum, columnNum, text, error}
 
-getLinesOf :: Pos -> Pos -> [CharInfo] -> String
-getLinesOf start end =
-  map fst
-    . filter (\(_, Info{infoLine}) -> infoLine >= start && infoLine <= end)
+data Sections = Sections
+  { leftLines :: [String]
+  , prefix :: String
+  , mid :: String
+  , suffix :: String
+  }
 
-spanToInfoSpan :: Span -> [CharInfo] -> (Info, Info)
-spanToInfoSpan (start, end) charInfos =
-  (posToInfo (Just start) charInfos, posToInfo end charInfos)
-
-posToInfo :: Maybe Pos -> [CharInfo] -> Info
-posToInfo pos charInfos = case pos of
-  Just pos ->
-    fromJust $
-      firstJust
-        (\(_, info) -> if infoPos info == pos then Just info else Nothing)
-        charInfos
-  Nothing -> snd $ last charInfos
-
--- FIXME non-unix lines
-
-getCharInfos :: String -> [CharInfo]
-getCharInfos =
-  snd
-    . foldl go (Info{infoPos = 0, infoLine = 0, infoColumn = 0}, [])
-  where
-    go :: (Info, [CharInfo]) -> Char -> (Info, [CharInfo])
-    go (info@Info{infoPos, infoLine, infoColumn}, infos) ch =
-      let info' = case ch of
-            '\n' ->
-              Info
-                { infoPos = infoPos + 1
-                , infoLine = infoLine + 1
-                , infoColumn = 0
-                }
-            _ ->
-              Info
-                { infoPos = infoPos + 1
-                , infoLine = infoLine
-                , infoColumn = infoColumn + 1
-                }
-       in (info', infos ++ [(ch, info)])
+getSections :: Span -> String -> Sections
+getSections (start, end) source =
+  let (left, right) = case end of
+        Just end -> splitAt (end + 1) source
+        Nothing -> ([], source)
+      suffix = case splitOn "\n" right of
+        [] -> ""
+        x : _ -> x
+      (left', mid) = splitAt start left
+      (prefix, leftLines) = case reverse $ splitOn "\n" left' of
+        [] -> ("", [])
+        x : xs -> (x, xs)
+   in Sections{leftLines, prefix, mid, suffix}
